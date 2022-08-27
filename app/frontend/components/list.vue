62c8be928e36d7102480b2e7f1cf3b6631d135ff
YPINPIN <yping611@gmail.com>
2022-08-27 21:34:14 +0800

    新增卡片 part 1

--- app/frontend/components/list.vue
+++ app/frontend/components/list.vue
@@ -3,12 +3,18 @@
     <h2 class="header">{{ list.name }}</h2>
 
     <div class="deck">
-      <Card v-for="card in list.cards" :card="card" :key="card.id"></Card>
+      <Card v-for="card in cards" :card="card" :key="card.id"></Card>
+
+      <div class="input-area">
+        <textarea class="content" v-model="content"></textarea>
+        <button class="button" @click="createCard">新增卡片</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Rails from '@rails/ujs'
 import Card from 'components/card'
 
 export default {
@@ -16,6 +22,35 @@ export default {
   props: ['list'],
   components: {
     Card,
+  },
+  data() {
+    return {
+      content: '',
+      cards: this.list.cards,
+    }
+  },
+  methods: {
+    createCard(event) {
+      event.preventDefault()
+
+      let data = new FormData()
+      data.append('card[list_id]', this.list.id)
+      data.append('card[name]', this.content)
+      Rails.ajax({
+        url: '/cards',
+        type: 'POST',
+        data,
+        dataType: 'json',
+        success: (resp) => {
+          console.log(resp)
+          this.cards.push(resp)
+          this.content = ''
+        },
+        error: (err) => {
+          console.log(err)
+        },
+      })
+    },
   },
 }
 </script>
@@ -30,6 +65,25 @@ export default {
 
   .deck {
     @apply .mt-2;
+  }
+
+  .input-area {
+    @apply .mt-2;
+    .content {
+      @apply .w-full .p-2 .rounded-sm;
+
+      &:focus {
+        @apply .outline-none;
+      }
+    }
+
+    .button {
+      @apply .px-3 .py-1 .font-semibold .text-sm .bg-blue-300 .rounded;
+
+      &:focus {
+        @apply .outline-none;
+      }
+    }
   }
 }
 </style>
