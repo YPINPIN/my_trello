5061eb08139e9a7e3527464a1fc4d3776c6849c9
YPINPIN <yping611@gmail.com>
2022-08-27 20:22:06 +0800

    改用 Vue.js 改寫

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend('DefinePlugin', new webpack.DefinePlugin({
+  // Drop Options API from bundle
+  __VUE_OPTIONS_API__: true,
+  __VUE_PROD_DEVTOOLS__: false
+}))
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
